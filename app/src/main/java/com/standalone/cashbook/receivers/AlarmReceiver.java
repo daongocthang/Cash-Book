5ae3c169359e0ca6b430e16bcc2dcfcadb63734c
wayfar3 <daongocthang.vn@gmail.com>
2024-08-08 17:00:08 +0700

    update

--- app/src/main/java/com/standalone/cashbook/receivers/AlarmReceiver.java
+++ app/src/main/java/com/standalone/cashbook/receivers/AlarmReceiver.java
@@ -6,9 +6,13 @@ import android.content.Intent;
 
 import com.standalone.cashbook.controllers.FireStoreHelper;
 import com.standalone.cashbook.models.PayableModel;
+import com.standalone.core.utils.DateTimeUtil;
 import com.standalone.core.utils.LogUtil;
 
+import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -34,10 +38,17 @@ public class AlarmReceiver extends BroadcastReceiver {
     private void doWork(Context context, List<PayableModel> payableList) {
         for (PayableModel model : payableList) {
             if (!model.isPaid()) continue;
-            if (model.getUpdatedAt().before(new Date())) {
+            int monthDiff = countMothsOfYear(new Date()) - countMothsOfYear(model.getUpdatedAt());
+            if (monthDiff != 0) {
                 model.setPaid(false);
                 helper.update(model.getKey(), model);
             }
         }
+    }
+
+    private int countMothsOfYear(Date dt) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dt);
+        return 12 * calendar.get(Calendar.YEAR) + calendar.get(Calendar.MONTH);
     }
 }
